7c69fe9dfa0d9af3d0cabfa7cf8dfdc4d5d65211
Texasvader16 <82550457+SnipingGod49@users.noreply.github.com>
2021-09-12 19:47:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+menu_list_array = [
+    "Chicken Tandoori Pizza" ,
+"Veg Supreame Pizza" ,
+"Panner Tikka Pizza" ,
+"Deluxe Veggie Pizza" ,
+"Veg Extravaganza Pizza"
+];
+
+function getmenu() {
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function addItem() 
+
+{
+    var html
+}
